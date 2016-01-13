f5934f430066beb6817215f0e0dcf7e8bca0887d
Maurice Knoop <mpjwknoop@gmail.com>
2016-01-13 12:36:30 +0100

    Moved painting from panel to new painter with more options

--- VoronoiGame/src/voronoigame/Util.java
+++ VoronoiGame/src/voronoigame/Util.java
@@ -5,14 +5,10 @@
  */
 package voronoigame;
 
-import java.awt.Color;
 import java.awt.Point;
 import java.util.Iterator;
 import java.util.List;
-import voronoigame.model.Cell;
-import voronoigame.model.FocusType;
 import voronoigame.model.GameState;
-import voronoigame.model.StationaryCell;
 
 /**
  *
@@ -20,56 +16,6 @@ import voronoigame.model.StationaryCell;
  */
 public class Util
 {
-    public static final Color COLOR_HEALTHY = new Color(0x93ed94);
-    public static final Color COLOR_INFECTED = new Color(0xff0000);
-    public static final Color COLOR_WHITE_CELL = new Color(0xffffff);
-    public static final Color COLOR_DEAD = new Color(0x888888);
-
-    public static Color getColorforCell(Cell cell)
-    {
-        Color returnColor = COLOR_WHITE_CELL;
-        if (cell.getType() == Cell.Type.DEAD)
-        {
-            return COLOR_DEAD;
-        }
-        if (cell.getClass() == StationaryCell.class)
-        {
-            StationaryCell stationaryCell = (StationaryCell) cell;
-            switch (stationaryCell.getType())
-            {
-                case INFECTED:
-                    return COLOR_INFECTED;
-                default:
-                    Color color = COLOR_HEALTHY;
-                    double currentRatio = cell.getCurrentAreaRatio();
-                    currentRatio = currentRatio < Cell.MAX_SCALE_FACTOR ? currentRatio : Cell.MAX_SCALE_FACTOR;
-                    
-                    double colorChangeFactor = (currentRatio - 1) / (Cell.MAX_SCALE_FACTOR - 1);
-                    
-                    int r = color.getRed();
-                    int g = color.getGreen();
-                    int b = color.getBlue();
-                    
-                    int rDiffMax = COLOR_DEAD.getRed() - r;
-                    int gDiffMax = COLOR_DEAD.getGreen() - g;
-                    int bDiffMax = COLOR_DEAD.getBlue() - b;
-                    
-                    r += (int)((double)rDiffMax * colorChangeFactor);
-                    g += (int)((double)gDiffMax * colorChangeFactor);
-                    b += (int)((double)bDiffMax * colorChangeFactor);
-                    
-                    returnColor = new Color(r, g, b);
-            }
-        }
-        
-        if (cell.getFocusType() == FocusType.HOVER || cell.getFocusType() == FocusType.DRAG)
-        {
-            returnColor = returnColor.brighter();
-        }
-        
-        return returnColor;
-    }
-
     public static Boolean isInCircle(Point point, Point circleCenter, double circleRadius)
     {
         return point.distance(circleCenter) <= circleRadius;
